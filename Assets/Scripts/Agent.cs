6aacb5393dbf94799e9b22f2b30b639ef3ae3a8f
Jacob Jenks <jacobjenks@gmail.com>
2015-12-11 14:19:38 -0700

    First commit

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Agent : MonoBehaviour
+{
+
+    private string agentType;
+    private Rigidbody body;
+    private float hp;
+    private Vector3 targetPos;
+    public float speed = 10;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        plan();
+    }
+
+    void FixedUpdate()
+    {
+        move();
+    }
+
+    void move()
+    {
+        Vector3 movement = Vector3.Normalize(targetPos - body.position) * speed * Time.deltaTime;
+        body.MovePosition(body.position + movement);
+    }
+
+    void plan()
+    {
+        targetPos.x = 10;
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject deerPrefab;
+
+    private static GameController game;
+    private static World world;
+    private static List<Object> agents;
+    private static float simSpeed = 1f;
+    private static int seed = 1234;
+
+
+	// Use this for initialization
+	void Start () {
+        if (game == null)
+            game = this;
+        else
+            Destroy(this);
+        initialize();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (agents.Count == 0)
+        {
+            Object n = Instantiate(deerPrefab, new Vector3(0, 0, -10), new Quaternion());
+            agents.Add(n);
+        }
+	}
+
+    private void initialize()
+    {
+        agents = new List<Object>();
+    }
+
+    public GameController getGame()
+    {
+        return this;
+    }
+
+    public float getSimSpeed()
+    {
+        return simSpeed;
+    }
+}
